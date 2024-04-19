1df29371624b233055fe4c5dfb4c8398da92d979
Jonathan O'Shea <jon.oshea@pobl.tech>
2024-04-19 18:30:03 +0100

    Ways to Improve improved

--- web/app/themes/estyn/resources/views/components/dot-link-arrow.blade.php
+++ web/app/themes/estyn/resources/views/components/dot-link-arrow.blade.php
@@ -1,8 +1,8 @@
-<div class="dot-link-arrow">
+<div class="dot-link-arrow {{ $wrapperClasses ?? '' }}">
     <a href="{{ $linkURL }}" class="stretched-link dot-link-arrow">
         <div class="d-flex align-items-center">
             <div class="rounded-circle {{ $bgColourClass }} p-3"></div>
-            <span class="d-block ms-3 me-1 flex-nowrap">{{ $text }}</span>
+            <span class="d-block ms-3 me-1">{{ $text }}</span>
             <i class="fa-sharp fa-regular fa-arrow-up-right"></i>
         </div>
     </a>
--- web/app/themes/estyn/resources/views/partials/ways-to-improve.blade.php
+++ web/app/themes/estyn/resources/views/partials/ways-to-improve.blade.php
@@ -1,53 +1,52 @@
+<div class="row">
+    <div class="col-12">
+        <h2>{{ __('Ways to improve', 'sage') }}</h2>
+    </div>
+</div>
 <div class="row justify-content-between">
     <div class="col-12 col-sm-4">
-        <h2>{{ __('Ways to improve', 'sage') }}</h2>
         <p>Estyn are here to help you improve your secondary school setting. Explore our vast wealth of improvement resources.</p>
     </div>
-    <div class="col-12 col-sm-6">
-        <div class="d-flex h-100 flex-column justify-content-center">
-            <div class="row gy-4">
-                <div class="col-4">
-                    @include('components.dot-link-arrow', [
-                        'linkURL' => '#',
-                        'text' => __('Numeracy', 'sage'),
-                        'bgColourClass' => 'bg-signpost-blue'
-                    ])
-                </div>
-                <div class="col-4">
-                    @include('components.dot-link-arrow', [
-                        'linkURL' => '#',
-                        'text' => __('Welsh Language', 'sage'),
-                        'bgColourClass' => 'bg-signpost-lime'
-                    ])
-                </div>
-                <div class="col-4">
-                    @include('components.dot-link-arrow', [
-                        'linkURL' => '#',
-                        'text' => __('Literacy', 'sage'),
-                        'bgColourClass' => 'bg-signpost-verylightbrown'
-                    ])
-                </div>
-                <div class="col-4">
-                    @include('components.dot-link-arrow', [
-                        'linkURL' => '#',
-                        'text' => __('Digital Skills', 'sage'),
-                        'bgColourClass' => 'bg-signpost-lightpink'
-                    ])
-                </div>
-                <div class="col-4">
-                    @include('components.dot-link-arrow', [
-                        'linkURL' => '#',
-                        'text' => __('Poverty', 'sage'),
-                        'bgColourClass' => 'bg-signpost-darkpink'
-                    ])
-                </div>
-                <div class="col-4">
-                    @include('components.dot-link-arrow', [
-                        'linkURL' => '#',
-                        'text' => __('Attendance', 'sage'),
-                        'bgColourClass' => 'bg-signpost-green'
-                    ])
-                </div>
+    <div class="col-12 col-lg-8 col-xl-7">
+        <div class="d-flex justify-content-between">
+            <div>     
+                @include('components.dot-link-arrow', [
+                    'linkURL' => '#',
+                    'text' => __('Numeracy', 'sage'),
+                    'bgColourClass' => 'bg-signpost-blue',
+                    'wrapperClasses' => 'mb-4'
+                ])
+                @include('components.dot-link-arrow', [
+                    'linkURL' => '#',
+                    'text' => __('Welsh Language', 'sage'),
+                    'bgColourClass' => 'bg-signpost-lime'
+                ])
+            </div>
+            <div>
+                @include('components.dot-link-arrow', [
+                    'linkURL' => '#',
+                    'text' => __('Literacy', 'sage'),
+                    'bgColourClass' => 'bg-signpost-verylightbrown',
+                    'wrapperClasses' => 'mb-4'
+                ])
+                @include('components.dot-link-arrow', [
+                    'linkURL' => '#',
+                    'text' => __('Digital Skills', 'sage'),
+                    'bgColourClass' => 'bg-signpost-lightpink'
+                ])
+            </div>
+            <div>
+                @include('components.dot-link-arrow', [
+                    'linkURL' => '#',
+                    'text' => __('Poverty', 'sage'),
+                    'bgColourClass' => 'bg-signpost-darkpink',
+                    'wrapperClasses' => 'mb-4'
+                ])
+                @include('components.dot-link-arrow', [
+                    'linkURL' => '#',
+                    'text' => __('Attendance', 'sage'),
+                    'bgColourClass' => 'bg-signpost-green'
+                ])
             </div>
         </div>
     </div>
