f965c8b95fcc526f48af36a345c4b2f443531aa8
Jonathan O'Shea <jon.oshea@pobl.tech>
2024-05-20 14:46:59 +0100

    Some of the homepage signposts are now linked up

--- web/app/themes/estyn/resources/views/partials/home-page-signposting.blade.php
+++ web/app/themes/estyn/resources/views/partials/home-page-signposting.blade.php
@@ -6,14 +6,14 @@
 			'svg' => asset('images/estyn-logo-icon-only-darkblue.svg'),
 			'title' => __('Learn more about Estyn', 'sage'),
 			'description' => __('Who we are and what we do', 'sage'),
-			'linkURL' => 'https://www.google.co.uk'
+			'linkURL' => App\get_permalink_by_template('template-about.blade.php')
 		],
 		[
 			'bgColourClass' => 'bg-signpost-lime',
 			'iconClasses' => 'fa-regular fa-chart-line-up',
 			'title' => __('Improve my setting', 'sage'),
 			'description' => __('Explore ways to improve with Estyn', 'sage'),
-			'linkURL' => 'https://www.google.co.uk'
+			'linkURL' => App\get_permalink_by_template('template-search.blade.php')
 		],
 		[
 			'bgColourClass' => 'bg-signpost-verylightbrown',
@@ -27,7 +27,7 @@
 			'iconClasses' => 'fa-solid fa-clipboard-check',
 			'title' => __('Find an inspection report', 'sage'),
 			'description' => __('View all our inspection reports', 'sage'),
-			'linkURL' => 'https://www.google.co.uk'
+			'linkURL' => App\get_permalink_by_template('template-inspection-report-search.blade.php')
 		],
 		[
 			'bgColourClass' => 'bg-signpost-darkpink',
@@ -41,7 +41,7 @@
 			'iconClasses' => 'fa-solid fa-users-rectangle',
 			'title' => __('Work for Estyn', 'sage'),
 			'description' => __('Our current vacancies and opportunities', 'sage'),
-			'linkURL' => 'https://www.google.co.uk'
+			'linkURL' => App\get_permalink_by_template('template-vacancies.blade.php')
 		]
 	]
 ])
