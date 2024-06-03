0f287502ef3bd7787cd60721dc8493fd7f8333e7
Jonathan O'Shea <jon.oshea@pobl.tech>
2024-06-03 12:27:23 +0100

    constants.php again

---
+++ web/app/mu-plugins/constants.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Old Estyn site files location
+ * 
+ * Paths appended to this should have 'files/' or 'private/files/' prepended to them
+ */
+add_action('init', function() {
+    if (!defined('ESTYN_OLD_FILES_URL')) {
+        define('ESTYN_OLD_FILES_URL', wp_upload_dir()['baseurl'] . '/estyn_old_files/');
+    }
+});
